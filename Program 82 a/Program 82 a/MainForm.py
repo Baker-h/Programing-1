e07227ff6a42f73feeaa2750b4c2e29e55a6feda
Baker-h <baker.h1@stu.janesville.k12.wi.us>
2024-10-31 09:37:19 -0500

    Update 10/31/2024 again

--- Program 82 a/Program 82 a/MainForm.py
+++ Program 82 a/Program 82 a/MainForm.py
@@ -10,6 +10,8 @@ class MainForm(Form):
     
     def InitializeComponent(self):
         self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
         self.SuspendLayout()
         # 
         # button1
@@ -19,13 +21,35 @@ class MainForm(Form):
         self._button1.Name = "button1"
         self._button1.Size = System.Drawing.Size(117, 55)
         self._button1.TabIndex = 0
-        self._button1.Text = "button1"
+        self._button1.Text = "Calculate"
         self._button1.UseVisualStyleBackColor = True
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(178, 14)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(117, 55)
+        self._button2.TabIndex = 1
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = True
+        # 
+        # button3
+        # 
+        self._button3.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(342, 14)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(117, 55)
+        self._button3.TabIndex = 2
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
         # 
         # MainForm
         # 
         self.BackColor = System.Drawing.SystemColors.ActiveCaption
         self.ClientSize = System.Drawing.Size(471, 453)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Name = "MainForm"
         self.Text = "Program 82 a"
