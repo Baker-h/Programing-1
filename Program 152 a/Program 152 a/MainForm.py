59e459c6c4a6b74375f5da6362db177948c93d97
Baker-h <baker.h1@stu.janesville.k12.wi.us>
2024-11-05 09:37:21 -0600

    Update 11/5/2024

---
+++ Program 152 a/Program 152 a/MainForm.py
@@ -0,0 +1,87 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._label1 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.GreenYellow
+        self._button1.Font = System.Drawing.Font("MS PGothic", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(12, 244)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(135, 46)
+        self._button1.TabIndex = 0
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.GreenYellow
+        self._button2.Font = System.Drawing.Font("MS PGothic", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(179, 244)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(135, 46)
+        self._button2.TabIndex = 1
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.GreenYellow
+        self._button3.Font = System.Drawing.Font("MS PGothic", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(349, 244)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(135, 46)
+        self._button3.TabIndex = 2
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.YellowGreen
+        self._label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        self._label1.Font = System.Drawing.Font("MV Boli", 26.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(67, 69)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(351, 45)
+        self._label1.TabIndex = 3
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.DarkOliveGreen
+        self.ClientSize = System.Drawing.Size(496, 302)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Name = "MainForm"
+        self.Text = "Program 152 a"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        Sum = repeat(3 + 3 for 3000
+        
+        self._label1.Text = str(Sum)
+
+    def Button2Click(self, sender, e):
+        pass
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
---
+++ String Stuff/String Stuff/Program.py
@@ -0,0 +1,13 @@
+﻿thing = "HELLO world Here is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.capitalize()) # Cap. first number
+print(thing.title()) # Every word cap.
+print(thing.isdigit())
+print(thing.find("world")) # Also see index and ringdex
+print(thing.count("E"))
+print(thing[::-1]) # Reverse
+print(thing.split(" "))
+print("Hello" * 5)
+
+input()
