76b458242c148ef3bf952f68fc43cd715cce6189
Baker-h <baker.h1@stu.janesville.k12.wi.us>
2024-11-12 09:38:33 -0600

    Update 11/12/2024

---
+++ Program 52a Class/Program 52a Class/Program.py
@@ -0,0 +1,20 @@
+﻿class Rectangle:
+    def__init__(self, length, width):   # Contructor
+        self.length = length
+        self.width = width
+        
+    def area(self):   # Class function
+        return self.length * self.width
+    
+    def perimiter(self):
+        return 2 * self.length + self.width
+    
+length = int(input("Enter length: "))
+width = int(input("Enter Width: "))
+r = Rectangle(length, width)   # Call class constructor to make new instance
+print("Area:", r.area())
+print("Perimiter:", r.perimiter())
+r,length *= 2
+print("Lenth * 2", r.length)
+
+input()
